02205438e59719b2504bcbed7130ef93a61510a8
Brandi Bermann <brandi.bermann@gmail.com>
2015-12-07 14:38:00 -0500

    adding form helpers and rspec

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -12,4 +12,13 @@ class PinsController < ApplicationController
     @pin = Pin.find_by_slug(params[:slug])
     render :show
   end
+  
+  def new
+    @pin = Pin.create(pins_params)
+  end
+  
+  private
+  def pin_params
+    params.require(:pins).permit(:title, :url, :slug, :text, :category_id)
+  end
 end
