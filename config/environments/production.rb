0eaa84c243651bb69bf7567f40f481c2c549975d
Brandi Bermann <brandi.bermann@gmail.com>
2015-12-11 17:23:35 -0500

    changed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['skillcrush-rails'],
-      :access_key_id => ENV['AKIAI4Y7CIUPPOFAYN2Q'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
