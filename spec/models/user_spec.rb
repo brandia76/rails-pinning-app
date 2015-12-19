46d7fb3c2239e6aa6351bb7904a065f0ba034d9b
Brandi Bermann <brandi.bermann@gmail.com>
2015-12-19 15:20:16 -0500

    add password validations, pin associations, login sessions, tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe User, type: :model do
   before(:all) do
-    @user = User.create(email: "coder@skillcrush", password: "password")
+    @user = FactoryGirl.build(:user)
   end
    
   after(:all) do
@@ -19,6 +19,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:last_name) } 
   it { should validate_presence_of(:email) } 
   it { should validate_presence_of(:password) } 
-  it { should validate_uniqueness_of(:password) } 
+ 
 
 end
